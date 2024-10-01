92f1dd81687e8f1fe9f802075e246e4a6bec18a7
Ameer Ghani <inahga@divviup.org>
2024-09-30 20:25:53 -0500

    miri fuzz

--- fuzz/fuzz_targets/compress.rs
+++ fuzz/fuzz_targets/compress.rs
@@ -1,28 +1,47 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
+#![cfg_attr(not(miri), no_main)]
+
+use libfuzzer_sys::{
+    arbitrary::{self, Arbitrary},
+    fuzz_target, Corpus,
+};
+
+use core::ffi::c_int;
 
 use zlib_rs::ReturnCode;
 
-fuzz_target!(|data: String| {
+#[derive(Debug, Arbitrary)]
+struct Input(String, c_int);
+
+fuzz_target!(|input: Input| -> Corpus { compress(input) });
+
+fn compress(input: Input) -> Corpus {
+    let Input(data, level) = input;
+
     // first, deflate the data using the standard zlib
-    let length = 8 * 1024;
-    let mut deflated = vec![0; length as usize];
-    let mut length = length as u64;
+    const LENGTH: usize = 8 * 1024;
+    let mut deflated = vec![0; LENGTH];
+    let mut length = LENGTH as u64;
     let error = unsafe {
-        libz_rs_sys::compress(
+        libz_rs_sys::compress2(
             deflated.as_mut_ptr().cast(),
             &mut length,
             data.as_ptr().cast(),
             data.len() as _,
+            level,
         )
     };
 
     let error = ReturnCode::from(error as i32);
-    assert_eq!(ReturnCode::Ok, error);
+    if (-1..=9).contains(&level) {
+        assert_eq!(ReturnCode::Ok, error);
+    } else {
+        assert_eq!(ReturnCode::StreamError, error);
+        return Corpus::Reject;
+    }
 
     deflated.truncate(length as usize);
 
-    let mut output = [0u8; 1 << 10];
+    let mut output = vec![0u8; LENGTH];
     let config = zlib_rs::inflate::InflateConfig { window_bits: 15 };
     let (output, error) = zlib_rs::inflate::uncompress_slice(&mut output, &deflated, config);
     assert_eq!(ReturnCode::Ok, error);
@@ -34,4 +53,27 @@ fuzz_target!(|data: String| {
     }
 
     assert_eq!(output, data.as_bytes());
-});
+    Corpus::Keep
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{fs::File, io::Read, path::PathBuf};
+
+    use libfuzzer_sys::arbitrary::{Arbitrary, Unstructured};
+    use rstest::rstest;
+
+    use crate::{compress, Input};
+
+    #[rstest]
+    #[cfg(miri)]
+    fn miri_corpus(#[files("corpus/compress/*")] path: PathBuf) {
+        let mut input = File::open(path).unwrap();
+        let mut buf = Vec::new();
+        input.read_to_end(&mut buf).unwrap();
+
+        let mut unstructured = Unstructured::new(&buf);
+        let input = Input::arbitrary(&mut unstructured).unwrap();
+        compress(input);
+    }
+}
